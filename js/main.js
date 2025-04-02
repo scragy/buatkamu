faf50f21c1787317ef6689e2104d6ea2e121119a
scragy <54729388+scragy@users.noreply.github.com>
2025-04-02 10:57:51 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('').split('')
+    const titles = ('HAPPY BIRTHDAY TIKA').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
